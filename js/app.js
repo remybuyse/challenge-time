4c594999c74689ef1e76dc6843ec499db8dbb7d4
Maxime Vasse <mallaxoz@gmail.com>
2017-02-09 14:15:11 +0100

    clearTimeout

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 function writeOk() {
+  console.info('writeOk');
+
   // Je récupère le div
   var counter = document.getElementById('counter');
 
@@ -7,4 +9,18 @@ function writeOk() {
 }
 
 // J'exécute la fonction writeOk dans 3000 ms
-setTimeout(writeOk, 3000);
+var timer = setTimeout(writeOk, 3000);
+
+// Stopper le timer
+function stopTimer() {
+  console.info('stopTimer');
+
+  // Avec clearTimeout, on peut arrêter le compte à rebours
+  clearTimeout(timer);
+}
+
+// Je récupère mon bouton
+var button = document.getElementById('stop');
+
+// J'écoute le clic
+button.addEventListener('click', stopTimer);
